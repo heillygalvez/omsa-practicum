52308dda26bbb0afec1d80c68bcd238628d849e0
heillygalvez <31016777+heillygalvez@users.noreply.github.com>
2025-07-07 23:53:19 -0600

    update line charts

--- pages/series.py
+++ pages/series.py
@@ -81,19 +81,18 @@ for year in years:
     socs = [obj['code'] for obj in uoa_df['socs'][uoa_idx]]
     aoi = wages[wages['OCC_CODE'].isin(socs)]
     aoi = aoi[aoi['year']==year]
-    wages_avg = aoi['A_MEDIAN'].mean()
+    wages_avg = aoi[['A_MEDIAN']].mean()
     wages_avgs.append(wages_avg)
     tot_emp_sum = aoi['TOT_EMP'].sum()
     tot_emp_sums.append(tot_emp_sum)
-agg_wages = zip(years, wages_avgs)
-agg_emps = zip(years, tot_emp_sums)
-agg_wages_df = pd.DataFrame(agg_wages, columns=['year', 'average_annual_wage'])
-agg_emps_df = pd.DataFrame(agg_emps, columns=['year', 'total_national_employment'])
+agg_emps = zip(years, wages_avgs, tot_emp_sums)
+agg_emps_df = pd.DataFrame(agg_emps, columns=['year', 'average_annual_wage', 'total_national_employment'])
 
 # Plot employment charts
-wages_fig = px.line(agg_wages_df, x='year', y='average_annual_wage')
-st.plotly_chart(wages_fig, use_container_width=True)
+wages_fig = px.line(agg_emps_df, x='year', y='average_annual_wage')
 emps_fig = px.line(agg_emps_df, x='year', y='total_national_employment')
+
+st.plotly_chart(wages_fig, use_container_width=True)
 st.plotly_chart(emps_fig, use_container_width=True)
 
-# STRETCH: Comparisons (agains other UOAs and other industries (area?))
+# STRETCH: Comparisons (against other UOAs and other industries (area?))
